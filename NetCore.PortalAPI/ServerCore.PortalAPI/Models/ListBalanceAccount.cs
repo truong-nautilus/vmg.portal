31765cbe2a18a6db056c559ca58fd4d82377df27
Phạm Thế Trưởng <gargles.violins-4i@icloud.com>
2025-12-15 15:50:13 +0700

    refactor: Thay đổi kiểu dữ liệu thuộc tính Balance từ decimal sang long trong ListBalanceAccount.

--- NetCore.PortalAPI/ServerCore.PortalAPI/Models/ListBalanceAccount.cs
+++ NetCore.PortalAPI/ServerCore.PortalAPI/Models/ListBalanceAccount.cs
@@ -5,7 +5,7 @@
         public string Name { get; set; }
         public string Code { get; set; }
         public int WalletId { get; set; }
-        public decimal Balance { get; set; }
+        public long Balance { get; set; }
         public int DecimalDigits { get; set; }
     }
 }
