7859e5ee0ab537365407f690c049a3ab2e13f31b
Phạm Thế Trưởng <gargles.violins-4i@icloud.com>
2025-12-31 08:21:30 +0700

    refactor: Thêm khả năng null cho các thuộc tính chuỗi trong mô hình LoginAccount.

--- NetCore.PortalAPI/Core/Domain/Models/LoginAccount.cs
+++ NetCore.PortalAPI/Core/Domain/Models/LoginAccount.cs
@@ -4,27 +4,27 @@
     {
         public int PlatformId { get; set; }
         public int MerchantId { get; set; }
-        public string IpAddress { get; set; }
-        public string UserName { get; set; }
-        public string Password { get; set; }
-        public string Uiid { get; set; }
-        public string CaptchaText { get; set; }
-        public string CaptchaToken { get; set; }
-        public string NickName { get; set; }
-        public string FacebookToken { get; set; }
-        public string FacebookId { get; set; }
-        public string VipCode { get; set; }
-        public string CampaignSource { get; set; }
-        public string AppleToken { get; set; }
-        public string AppleId { get; set; }
-        public string AppleEmail { get; set; }
-        public string DeviceName { get; set; }
+        public string? IpAddress { get; set; }
+        public string? UserName { get; set; }
+        public string? Password { get; set; }
+        public string? Uiid { get; set; }
+        public string? CaptchaText { get; set; }
+        public string? CaptchaToken { get; set; }
+        public string? NickName { get; set; }
+        public string? FacebookToken { get; set; }
+        public string? FacebookId { get; set; }
+        public string? VipCode { get; set; }
+        public string? CampaignSource { get; set; }
+        public string? AppleToken { get; set; }
+        public string? AppleId { get; set; }
+        public string? AppleEmail { get; set; }
+        public string? DeviceName { get; set; }
         public int IsLoginUID { get; set; }
         public int LocationID { get; set; }
-        public string PreFix { get; set; }
-        public string Email { get; set; }
-        public string WAddress { get; set; }
-        public string PartnerUserID { get; set; }
+        public string? PreFix { get; set; }
+        public string? Email { get; set; }
+        public string? WAddress { get; set; }
+        public string? PartnerUserID { get; set; }
 
     }
 }
